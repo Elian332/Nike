93582bd98d5c45071f1c5c9a7e5f869cdcafc3f5
Elian332 <78801982+Elian332@users.noreply.github.com>
2021-03-02 20:49:32 -0400

    Add files via upload

---
+++ inicio.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<head>
+<title>Nike</title>
+</head>
+<body>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Logo_NIKE.svg/1280px-Logo_NIKE.svg.png" width="400" height="120"<DIV ALIGN="RIGHT"</DIV>
+<font size="10" face="Algerian"><DIV ALIGN="LEFT"><h1>NIKE</h1></DIV>
+<a name="enlaces_internos"></a>
+<hr size="4" width="1000" color="black" noshade>
+<marquee Behavior direction="left" width=20%>BIENVENIDOS</marquee> 
+<font color="blue" face "arial" size="7"><DIV ALIGN="Right"><a href="pagina segundaria.php">Entrena con nosotros</a></DIV>
+<DIV ALIGN="center"><em><p>P&AacuteGINA OFICIAL</p></em></DIV>
+	<hr size="4" color="White" noshade>
+<DIV ALIGN="CENTER"><font color="white"><h3>INFORMACI&OacuteN</h3>
+	<hr size="4" color="White" noshade>
+	<font color="blue" face="Monotype Corsiva" align="left"><p>Somos una empresa multinacional estadounidense dedicada al dise&ntildeo, desarrollo, fabricaci&oacuten y comercializaci&oacuten de equipamiento deportivo: balones, calzado, ropa, equipo, accesorios y otros art&iacuteculos deportivos..</p></font>
+		<hr size="4" color="White" noshade>
+	<font color="white" align="center"><h3>HISTORIA</h3>
+	<hr size="4" color="White" noshade>
+<font color="green" align="left"><strong><em><p>Fundada por el deportista de la Universidad de Oreg&oacuten Phil Knight y su entrenador, William Jay "Bill" Bowerman. La empresa comenz&oacute distribuyendo calzado de la firma Onitsuka Tiger (actualmente ASICS) hasta 1971, cuando BRS lanz&oacute su primer producto propio, con el emblema de la marca disea&ntildedo por Carolyn Davidson.</p></em></strong>
+	<hr size="4" color="White" noshade>
+<font color="white" align="center"><h3>VISI&OacuteN</h3>
+	<hr size="4" color="White" noshade>
+<font color="red" align="left"><big> <p>Ser la mejor empresa nacional e internacional destac&aacutendonos por la buena calidad, servio y comodidad a nuestros clientes.</big><small><font color="orange">Queremos que nuestro producto sea vendido y utilizado en todas las partes del mundo.</p></small></font>
+	<hr size="4" color="White" noshade>
+<font color="white" align="center"><h3>ACCESORIOS NUEVOS DISPONIBLES</h3>
+	<hr size="4" color="White" noshade>
+<ul>
+	<font color="black"><li>Nuevos conjuntos de AIRFORCE.</li>
+	<li>Sudaderas Nike hombre y mujer.</li>
+	<li>Nike air MAX todos los sizes diponibles.</li>
+	</ul>
+<br>
+
+	<hr size="4" color="White" noshade>
+<font color="white"><h3>PRODUCTOS CON MAYORES VENTAS EN LAS INSTALACIONES</font>	</h3>
+	<hr size="4" color="White" noshade>
+<hr color="black">
+<font color="black"><h2><strong>Nike Air Max 270</h2></strong></font>
+	<hr color="black">
+	<img src="https://www-lavanguardia-com.cdn.ampproject.org/i/s/www.lavanguardia.com/files/content_image_intermediate_filter/uploads/2020/03/05/5fa8fe501175d.jpeg" width="950" height="900">
+		<hr color="black">
+	<font color="black"><h2><strong>Nike Air Force 1 Low</h2></strong></font>
+	<hr color="black">
+	<img src="https://www-lavanguardia-com.cdn.ampproject.org/i/s/www.lavanguardia.com/files/content_image_intermediate_filter/uploads/2020/03/05/5fa90a4c53b68.jpeg" width="950" height="900">
+		<hr color="black">
+		<font color="black"><h2><strong>Nike Tanjun</h2></strong></font>
+	<hr color="black">
+	<img src="https://www-lavanguardia-com.cdn.ampproject.org/i/s/www.lavanguardia.com/files/content_image_intermediate_filter/uploads/2020/03/05/5fa90a04ab9ce.jpeg" width="950" height="900">
+	<font color="black">
+	<hr color="black">
+	<font color="black"><h2><strong>Nike Air Max 97</h2></strong></font>
+	<hr color="black">
+	<img src="https://www-lavanguardia-com.cdn.ampproject.org/i/s/www.lavanguardia.com/files/content_image_intermediate_filter/uploads/2020/03/05/5fa90838e2323.jpeg" width="950" height="900">
+	<font color="black">
+	<hr color="black">
+	
+	
+		
+	<hr size="4" color="White" noshade>
+
+
+<font color="white"><em><strong><a href="#enlaces_internos">Volver al inicio de la p&aacutegina</a></font></em></strong>
+<Body BgColor= "#B0C4DE">
+</body>
+</html>
